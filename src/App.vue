297651a077a30a0eb336756bc8cbd60b8bc25b58
Phive <phivedec@gmail.com>
2025-05-15 22:20:18 +0800

    test change 3

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
+  height: 5em;
+  padding: 2.5em;
   will-change: filter;
   transition: filter .2s;
 }
